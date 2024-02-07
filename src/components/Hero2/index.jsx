c716c26ce34543cf5ead103478a8e6eb2ab59813
DAppArtisan <sandeep@unilend.finance>
2024-02-07 16:13:28 +0530

    video update

--- src/components/Hero2/index.jsx
+++ src/components/Hero2/index.jsx
@@ -30,7 +30,7 @@ const hero = () => {
         preload="metadata"
       >
         <source src="/assets/Lp.mp4" type="video/mp4" />
-        <source src="/assets/ip.ogg" type="video/ogg" />
+        {/* <source src="/assets/ip.ogg" type="video/ogg" /> */}
         <Image src="/assets/hero-unilend.png" alt="poster" width={100} height={100}/>
       </video> 
     <div className="hero-info-container wrapper"> 
